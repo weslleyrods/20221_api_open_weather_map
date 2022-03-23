ab5b6e94f38af29908d5927eb519286ab7558c33
Weslley Rodrigues <weslley.rdgs@gmail.com>
2022-03-23 09:00:52 -0300

    finalizada a aplicacao da api, retornando temperatura min/max, descricao e data/hora da previsao do tempo

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
-//api.openweathermap.org/data/2.5/forecast?q=Sao%20Paulo&appid=f74a20774a615d3a7cac895e4221ea84
+require('dotenv').config()
+const axios = require('axios')
+
+
+// console.log(process.env.APP_KEY) 
+// console.log(process.env.UNITS) 
+// console.log(process.env.LANGUAGE) //porque lang pegaria a variavel lang da máquina, e não o idioma estabelido no env
+
+
+// const PROTOCOL = process.env.PROTOCOL
+// const BASE_URL = process.env.BASE_URL
+
+const {PROTOCOL, BASE_URL, APPID, UNITS, CNT, Q, LANGUAGE} = process.env
+
+
+const url = `${PROTOCOL}://${process.env.BASE_URL}?q=${Q}&appid=${APPID}&cnt=${CNT}&lang=${LANGUAGE}&units=${UNITS}`
+//console.log(url)
+
+// const resultado = axios.get(url)
+// console.log(resultado)
+
+axios
+    .get(url)
+    .then(res => {
+        //console.log(res)
+        return res.data
+    })
+    .then(res =>{
+        //console.log(resultado)
+        return res[`list`]
+    })
+    .then(res =>{
+        //console.log(res)
+        //Desafio - exibir temperatura mínima, máxima e descrição de cada previsão
+        //Plus - adicionei a data/hora também
+        for(let previsao of res){
+            const{temp_min, temp_max} = previsao.main
+            const{description} = previsao.weather[0]
+
+            console.log(`
+            Temperatura máxima: ${temp_max}
+            Temperatura mínima: ${temp_min}
+            Descrição: ${description} 
+            Data e hora da previsão: ${previsao.dt_txt}`)
+            // console.log("Temperatura minima: "+temp_min)
+            // console.log("Temperatura mmáxima: "+temp_max)
+            // console.log("Descrição: "+description)
+            // console.log("Data e hora da previsão: "+previsao.dt_txt)
+        }        
+    })
 
 
